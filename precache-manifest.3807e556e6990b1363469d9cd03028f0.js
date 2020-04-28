e54c6c973ceab2e8c23e89ffd07520b53a30b90a
cvspinelli <cvspinelli09@gmail.com>
2020-04-28 18:22:29 +0100

    Updates

---
+++ precache-manifest.3807e556e6990b1363469d9cd03028f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ed1b0cd7494e5bacaf0a9c2a1be8ad",
+    "url": "/demographics/index.html"
+  },
+  {
+    "revision": "2cddf0d17773cc983513",
+    "url": "/demographics/static/css/main.29061d9c.chunk.css"
+  },
+  {
+    "revision": "fa065e01c996a725ba3a",
+    "url": "/demographics/static/js/2.68a9a2a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demographics/static/js/2.68a9a2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cddf0d17773cc983513",
+    "url": "/demographics/static/js/main.9a421a84.chunk.js"
+  },
+  {
+    "revision": "0456b48df4dc424b86a3",
+    "url": "/demographics/static/js/runtime-main.55d533da.js"
+  },
+  {
+    "revision": "e5abcd2ed0726d43acab5bbd12827fb7",
+    "url": "/demographics/static/media/hor-logo-small.e5abcd2e.svg"
+  }
+]);
